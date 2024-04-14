15f7d005d01ce6a2836f1e019dd5f356fde5a71e
BlackBear <fanat991@ukr.net>
2024-04-14 22:58:46 +0300

    Add project files.

---
+++ FlexPront_Console/Manager/IPrinterManager.cs
@@ -0,0 +1,26 @@
+﻿using FlexPrint_Console.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexPrint_Console.Manager
+{
+	public interface IPrinterManager
+	{
+
+		void LoadDataFromDatabase();
+		void AddPrinter<T>(T printer) where T : Printer;
+		void RemovePrinter(string productCode);
+		void EditPrinter<T>(string productCode, T newPrinterData) where T : Printer;
+		void SortPrintersByPrice();
+		List<LaserPrinter> GetLaserPrinters();
+		List<InkjetPrinter> GetInkjetPrinters();
+		List<Printer> GetPrintersByManufacturer(string manufacturer);
+		List<Printer> GetHomePrinters();
+		List<Printer> GetOfficePrinters();
+	
+	}
+
+}
---
+++ FlexPront_Console/Program.cs
@@ -0,0 +1,44 @@
+﻿using FlexPrint_Console.DB;
+using FlexPrint_Console.Enum;
+using FlexPrint_Console.Manager;
+using FlexPrint_Console.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Reflection;
+
+namespace FlexPrint_Console
+{
+	public class FlexPrint_Console
+	{
+
+
+		static void Main()
+		{
+
+			var configuration = new ConfigurationBuilder()
+		.SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+		.AddJsonFile("appsettings.json")
+		.Build();
+
+			var dbContext = new PrinterDbContext(configuration);
+			dbContext.Database.EnsureCreated();
+
+
+			IPrinterManager printerManager = new PrinterManager(configuration);
+
+			LaserPrinter l1 = new LaserPrinter()
+			{
+				LaserType = LaserPrinterType.Gas,
+				PrinterSize = MaxPrinterSize.A4,
+				Manufacturer = "HP",
+				Model = "ui53",
+				Price = 16000,
+				Purpose = PrinterPurpose.Home,
+			};
+			printerManager.EditPrinter("M0QBPZ", l1);
+
+
+		}
+	}
+}
